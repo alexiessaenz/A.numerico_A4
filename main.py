0bcde742bd7b5f933122d6bfd2aab45b3c67b7b5
moises alexi saenz <00400815@uca.edu.sv>
2020-05-01 02:55:51 +0000

    run yet..

--- main.py
+++ main.py
@@ -5,20 +5,16 @@ from decimal import *
 def f(alpha):
     return ((3*pi)/2)*quad(lambda x: cos((3*pi*x)/2)/((3*pi*x)/2)**(alpha),0,1)[0]
 
-def df(alpha): 
+def df(alpha):
     return (-(3*pi)/2)*quad(lambda x: cos((3*pi*x)/2)*log((3*pi*x)/2)*((3*pi*x)/2)**(-alpha),0,1)[0]
-    
 
 def newton(p0, TOL, Nmax):
-    p=0.0
     for i in range(Nmax):
-	    p=p0-f(p0)/df(p0)
-	    print(str(i)+"\t"+str(p0)+"\t"+str(p)+"\t"+str(abs(p0-p))+"\n")
-      if abs(p-p0) < TOL:
-        break
-    p0 = p				
-	
+        p=p0-f(p0)/df(p0)
+        #getcontext().prec = 5
+        print(str(i)+"\t"+str(p0)+"\t"+str(p)+"\t"+str(abs(p0-p)))
+        if abs(p-p0) < TOL:
+            break
+        p0 = p
 
-getcontext().prec = 15
-
-newton(pi/4, 10**-10, 40)
+newton(pi/4, 10**-15, 40)
